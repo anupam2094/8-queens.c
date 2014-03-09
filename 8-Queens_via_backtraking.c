0fd2c0c1d3d6bf5b9fa1c2f1b7585ecc110a0564
Anupam Satsangi <anupam2094@users.noreply.github.com>
2014-03-09 17:19:16 +0530

    Create 8-Queens_via_backtraking.c
    Backtraking is a very effective method that will necessarily give the solution of the problem if it exists. In this the first start with initial condition is changed in backward fashion when we left with no choice means we cant continue.

---
+++ 8-Queens_via_backtraking.c
@@ -0,0 +1,85 @@
+void prntArena()
+{
+	int i, j;
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%d  ", arena[i][j]);
+		}
+		printf("\n");
+	}
+}
+#include<stdio.h>
+
+//For backtracking
+int willAttack(int f1, int f2, int m1, int m2)
+{
+	int m, n;
+	m = f1 + f2;
+	n = f1 - f2;
+	if (f1 != m1 && f2 != m2)
+	{
+		if (m1 - m2 != n && m1 + m2 != m)
+			return(1);
+	}
+	return 0;
+}
+
+int willBeAttacked(int x, int y)
+{
+	int i, j, k = 0;
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			if (i == x && j == y)
+				continue;
+			else if (arena[i][j] == 1)
+			{
+				if (!willAttack(i, j, x, y))
+				{
+					k = 1;
+				}
+			}
+		}
+	}
+	return k;
+}
+
+void main()
+{
+	printf("enter location of first queen in first coulmn:");
+	int i, j, x, y, z, n;
+	scanf_s("%d", &n);
+	int stack[8];
+	initilize();
+	arena[n][0] = 1;
+	//printf("%d\n",willBeAttacked(2, 6));
+	stack[0] = n;
+	i = 1; j = 1;
+	while (j < 8)
+	{
+		if (i == 8)
+		{
+			j--;
+			i = stack[j];
+			arena[i][j] = 0;
+			i++;
+		}
+		else if( willBeAttacked(i, j))
+		{
+			i++;
+		}
+		else
+		{
+			arena[i][j] = 1;
+			stack[j] = i;
+			j++;
+			i = 0;
+		}
+	}
+
+	prntArena();
+
+}
